9ba4807bc00ea6fa21fb69a2fe7be358201e1b0b
shane <shane@mitrealityhack.com>
2024-01-25 22:48:40 -0500

    Reconcile migrations
    Squashed commit of the following:
    
    commit 52f11313e775bcb9c24e0e5f5d798bf21a78f489
    Author: shane <shane@mitrealityhack.com>
    Date:   Thu Jan 25 22:48:25 2024 -0500
    
        Reconcile migrations made asynchronously

--- infrastructure/management/commands/assign_destiny_tables.py
+++ infrastructure/management/commands/assign_destiny_tables.py
@@ -1,7 +1,7 @@
 from itertools import zip_longest
 
-from django.db import transaction
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from infrastructure.models import DestinyTeam, Table, Track
 
