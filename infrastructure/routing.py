2f0a155f067f59dd03a38478c3fcea5ba1709fdb
shane <shane@mitrealityhack.com>
2024-02-07 23:07:54 -0500

    Start from scratch

--- infrastructure/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-
-from infrastructure import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/lighthouses/$", consumers.LightHousesConsumer.as_asgi()),
-    re_path(r"ws/lighthouse/(?P<table>\w+)/$",
-            consumers.LightHouseByTableConsumer.as_asgi()),
-]
